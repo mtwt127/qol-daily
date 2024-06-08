d49a6cd9a24faee3441d5eeed6ac71058ab258f0
Wataru Matsunaga <mtngwtr@gmail.com>
2024-06-08 18:06:33 +0900

    投稿機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def addDaily
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to '/daily'
+  end
+
+  private
+    def post_params
+      params.permit(:title, :body, :qol)
+    end
 end
